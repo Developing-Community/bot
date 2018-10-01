ca1e0a0aef3b42d2b9f7a1d448f0bc8bca117465
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-10-01 16:43:08 +0330

    getting token from local api

--- dev_communtiy_bot.py
+++ dev_communtiy_bot.py
@@ -2,6 +2,7 @@ import requests
 import telepot
 from telepot.loop import MessageLoop
 import time
+import json
 from pprint import pprint
 from config import TOKEN
 start_msg = '''
@@ -19,9 +20,17 @@ def handle(msg) :
             if msg['text'] == '/start':
                 bot.sendMessage(chat_id, start_msg, 'Markdown')
             elif msg['text'] == '/login':
-                user_id = msg['from']['id']
-                verify_token = requests.post('localhost/api/bot/get_token', user_id)
-                bot.sendMessage(chat_id, 'https://dev-community.ir/verify-bot?token=' + verify_token)
+                try:
+                    data = json.dumps({
+                    'telegram_user_id': msg['from']['id']
+                    })
+                    response = requests.post('http://localhost/api/bot/create-token/',
+                                             headers={"Content-type": "application/json"},
+                                             data = data)
+                    bot.sendMessage(chat_id, 'https://dev-community.ir/verify-bot?token=' + str(response.json()['user_token']))
+                except Exception as e:
+                    print(e)
+                    bot.sendMessage(chat_id, 'خطایی پیش آمده. لطفا دقایقی دیگر مجددا سعی کنید')
     elif chat_type in [u'group', u'supergroup'] :
         if 'left_chat_member' in msg or 'new_chat_member' in msg or 'new_chat_members' in msg :
             bot.deleteMessage((chat_id, msg['message_id']))
