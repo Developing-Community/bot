fdde53596692641dab818e712c191fe71211220d
Ali Mahdiyar <alimahdiyar77@gmail.com>
2018-11-11 01:19:50 +0330

    reformat

--- dev_communtiy_bot.py
+++ dev_communtiy_bot.py
@@ -16,23 +16,21 @@ application = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-
 def logadd(text):
-
     f = open(os.path.join(BASE_DIR, "bot.log"), "a")
     f.write(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S] ") + text + '\n')
     f.close()
 
 
 def keyboard_maker(keyboard_texts):
-
     mykb = []
     for x in keyboard_texts:
         l = []
         for y in x:
-            l.append(KeyboardButton(text = y))
+            l.append(KeyboardButton(text=y))
         mykb.append(l)
     return mykb
+
 
 @application.route('/send-message/', methods=['POST'])
 def send_message():
@@ -42,14 +40,15 @@ def send_message():
         bot.sendMessage(chat_id,
                         data['message'],
                         'Markdown',
-                            reply_markup = ReplyKeyboardMarkup(
-                                keyboard=keyboard
-                            ),
+                        reply_markup=ReplyKeyboardMarkup(
+                            keyboard=keyboard
+                        ),
                         )
     response = {
         'status': 'success'
     }
     return jsonify(response)
+
 
 def handle_pv(msg):
     data = json.dumps({
@@ -57,15 +56,16 @@ def handle_pv(msg):
     })
     response = requests.post(url=BOT_API_HOST_URL + '/api/bot/handle-pv/',
                              headers={"Content-type": "application/json"},
-                             data = data).json()
+                             data=data).json()
 
     keyboard = keyboard_maker(response['keyboard'])
     bot.sendMessage(response['chat_id'],
                     response['message'],
                     'Markdown',
-                    reply_markup = ReplyKeyboardMarkup(
+                    reply_markup=ReplyKeyboardMarkup(
                         keyboard=keyboard
                     ))
+
 
 def handle_gp(msg):
     data = json.dumps({
@@ -73,26 +73,27 @@ def handle_gp(msg):
     })
     response = requests.post(url=BOT_API_HOST_URL + '/api/bot/handle-gp/',
                              headers={"Content-type": "application/json"},
-                             data = data).json()
+                             data=data).json()
 
 
-def handle(msg) :
+def handle(msg):
     global start_msg, users
     content_type, chat_type, chat_id = telepot.glance(msg)
     if chat_type == u'private' and content_type == 'text':
         handle_pv(msg)
 
-    elif chat_type in [u'group', u'supergroup'] :
-        if msg['from']['is_bot'] or 'left_chat_member' in msg or 'new_chat_member' in msg or 'new_chat_members' in msg :
+    elif chat_type in [u'group', u'supergroup']:
+        if msg['from']['is_bot'] or 'left_chat_member' in msg or 'new_chat_member' in msg or 'new_chat_members' in msg:
             bot.deleteMessage((chat_id, msg['message_id']))
-        if 'new_chat_member' in msg :
-            if msg['new_chat_member']['is_bot'] :
-                try :
+        if 'new_chat_member' in msg:
+            if msg['new_chat_member']['is_bot']:
+                try:
                     bot.kickChatMember(chat_id, msg['new_chat_member']['id'])
                     bot.kickChatMember(chat_id, msg['from']['id'])
-                except : # admin did it !
-                    return # so it's OK !
+                except:  # admin did it !
+                    pass  # so it's OK !
         handle_gp(msg)
+
 
 if PROXY:
     telepot.api.set_proxy(PROXY)
@@ -101,4 +102,3 @@ bot = telepot.Bot(TOKEN)
 MessageLoop(bot, handle).run_as_thread()
 if __name__ == "__main__":
     application.run(host='localhost')
-
